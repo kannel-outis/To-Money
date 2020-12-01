3a8060af21ecd6b60e061d786d8b5eecc03e0ae0
khaniel-outis <emirbadoo@gmail.com>
2020-12-01 12:27:09 -0800

    tomoney

--- example/main.dart
+++ example/main.dart
@@ -1,9 +1,9 @@
 import "../lib/tomoney.dart";
 
 void main() {
-  var money = '123456789.00'.toMoney();
-  var money2 = '12345678900000'.toMoney();
-  var money3 = '123456789000000000000000000000000.00'.toMoney();
+  var money = '12345607890.00'.toMoney();
+  var money2 = '1000000000'.toMoney();
+  var money3 = '100.0033333'.toMoney();
   print(money);
   print(money2);
   print(money3);
--- lib/tomoney.dart
+++ lib/tomoney.dart
@@ -2,30 +2,23 @@
 
 extension ToMoney on String {
   String toMoney() {
-    int number = 0;
     if (!this.contains(".")) {
-      List<String> characters = this.toString().split("").reversed.toList();
+      List<String> characters = this.split("").reversed.toList();
       for (var i = 0; i < characters.length; i++) {
         if (i % 4 == 0) {
           characters.insert(i, ",");
-          number++;
         }
       }
-      return characters.reversed
-          .join()
-          .substring(0, this.toString().length + number - 1);
+      return characters.reversed.join().substring(0, characters.length - 1);
     } else {
       List<String> char = this.split(".").toList();
       List<String> characters = char[0].split("").reversed.toList();
       for (var i = 0; i < characters.length; i++) {
         if (i % 4 == 0) {
           characters.insert(i, ",");
-          number++;
         }
       }
-      return characters.reversed
-              .join()
-              .substring(0, this.length + number - char[1].length - 2) +
+      return characters.reversed.join().substring(0, characters.length - 1) +
           (".${char[1]}");
     }
   }
